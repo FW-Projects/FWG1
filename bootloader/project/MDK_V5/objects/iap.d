67ab39e48ccb0389950b3cbfa9c477508ceebec3
Witt <Witt@forward.com>
2025-09-19 13:51:46 +0800

    change iap

---
+++ bootloader/project/MDK_V5/objects/iap.d
@@ -0,0 +1,22 @@
+.\objects\iap.o: ..\src\iap.c
+.\objects\iap.o: ..\inc\iap.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\device_support\at32f415.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\core_support\core_cm4.h
+.\objects\iap.o: D:\Work Program\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\core_support\cmsis_version.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\core_support\cmsis_compiler.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\core_support\cmsis_armcc.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\core_support\mpu_armv7.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\device_support\system_at32f415.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_def.h
+.\objects\iap.o: ..\inc\at32f415_conf.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_crc.h
+.\objects\iap.o: ..\..\libraries\cmsis\cm4\device_support\at32f415.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_crm.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_debug.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_flash.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_gpio.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_misc.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_pwc.h
+.\objects\iap.o: ..\..\libraries\drivers\inc\at32f415_usart.h
+.\objects\iap.o: ..\inc\flash.h
